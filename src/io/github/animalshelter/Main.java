99fee978d0bb38a00e214db8f02d63811488f27c
DaltonSenseman <drsenseman4328@eagle.fgcu.edu>
2019-09-23 23:29:59 -0400

    Database has been set up using H2, with baseline Tables set up to append onto, as more information is needed.

--- src/io/github/animalshelter/Main.java
+++ src/io/github/animalshelter/Main.java
@@ -6,18 +6,50 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("animalshelter.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    // establish connection to database.
+    initializeDB();
 
+    Parent root = FXMLLoader.load(getClass().getResource("animalshelter.fxml"));
+    primaryStage.setTitle("Animal Shelter");
+    primaryStage.setScene(new Scene(root, 300, 275));
+    primaryStage.show();
+  }
 
-    public static void main(String[] args) {
-        launch(args);
+  private void initializeDB() {
+    final String JDBC_DRIVER = "org.h2.Driver";
+    final String DB_URL = "jdbc:h2:./res/data";
+    final String USER = "";
+    final String PASS = "";
+    Connection conn = null;
+    Statement stmt = null;
+
+    //This is a test SQL query to validate the DB is functioning this is to be removed once
+    //SQL query's are mapped to a button / dialog in the GUI program.
+    try {
+      Class.forName(JDBC_DRIVER);
+      conn = DriverManager.getConnection(DB_URL, USER, PASS);
+      stmt = conn.createStatement();
+      String test1 = "INSERT INTO ANIMAL(NAME, SPECIES) "
+          + "VALUES ('Felix','TomCat')";
+      stmt.executeUpdate(test1);
+      stmt.close();
+      conn.close();
+    } catch (Exception ex) {
+      ex.printStackTrace();
     }
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
